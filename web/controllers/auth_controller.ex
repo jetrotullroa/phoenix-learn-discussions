447801fe5e9d21b7e8403752c2aa430663263aaf
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-02-23 17:16:35 +0800

    setup signout and signin using github

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -12,6 +12,12 @@ defmodule Discuss.AuthController do
     signin(conn, changeset)
   end
 
+  def signout(conn, _params) do
+     conn
+     |> configure_session(drop: true)
+     |> redirect(to: topic_path(conn, :index))
+  end
+
 
   #PRIVATE FUNCTIONS
   defp signin(conn, changeset) do
